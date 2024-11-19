7437717daeedc8dc46aa3338d82fa9a9d7d2fae7
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-19 17:40:55 +0100

    Update RemoveTransactionByIdUnitTest.java

--- test/java/RemoveTransactionByIdUnitTest.java
+++ test/java/RemoveTransactionByIdUnitTest.java
@@ -3,9 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class RemoveTransactionByIdUnitTest {
 
-    // removeTransactionById(id)
-    // 1. Ако имам транзакция с това ид - премахвам транзакцията
-    // 2. Ако нямам транзакция с това ид - throw IllegalArgumentException
+ 
 
     @Test
     public void whenTransactionExist_thenRemoveIt() {
@@ -15,11 +13,10 @@ public class RemoveTransactionByIdUnitTest {
         Transaction transactionA = new TransactionImpl(1, TransactionStatus.SUCCESSFUL, "Vik", "Dani", 200.00);
         database.add(transactionA);
 
-        // Имам 1 транзакция
         Assertions.assertEquals(1, database.getCount());
-        // Премахвам тази транзакция
+      
         database.removeTransactionById(1);
-        // Имам 0 транзакция
+      
         Assertions.assertEquals(0, database.getCount());
     }
 
